55ccc51287a14f03595c9e03d649c1cd80df3ac0
gabrielle johnson <gabriellejohnson784@gmail.com>
2023-11-04 14:47:51 -0500

    Added javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    const subscribe = document.querySelector(".btn");
+
+    subscribe.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const email = document.getElementById("email");
+        const isvalidemail = email.value.trim(); 
+
+        if (isvalidemail === "") {
+            document.querySelector(".message").textContent = "Please enter a valid email address.";
+        } else {
+            document.querySelector(".message").textContent = `Thank you! Your email address ${enteredEmail} has been added to our mailing list!`;
+        }
+    });
+    
+});
